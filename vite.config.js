a7be647af4191e54ed7534e25e85f405c01c8d26
dangkei <dangkei@163.com>
2025-08-11 22:28:17 +0800

    chore: add .gitignore

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/CloudMusic/',
+  build: {
+    output: {
+      assetFileNames: 'assets/[name]-[hash:4].[ext]',
+      chunkFileNames: 'assets/[name]-[hash:4].js',
+      entryFileNames: 'assets/[name]-[hash:4].js'
+      }
+    }
 });
