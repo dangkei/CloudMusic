1c120fef68d757db048fc69d90e51c69f93235c6
dangkei <dangkei@163.com>
2025-08-12 16:06:30 +0800

    更新 main页面

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,18 +3,20 @@ import SongCard from '../components/SongCard'
 import { usePlayer } from "../context/PlayerContext";
 import { Song } from "../context/PlayerContext";
 
+
 const Home = () => {
+  const baseUrl = import.meta.env.BASE_URL;
     const { playSong } = usePlayer();
-      const songs = [
-    { id: 1, title: "Lost Stars", artist: "Adam Levine", image: "/images/song1.jpeg", audio: "/audio/song2.mp3" },
-    { id: 2, title: "Blinding Lights", artist: "The Weeknd", image: "/images/COVER2.jpeg", audio: "/audio/song1.mp3" },
-    { id: 3, title: "Shape of You", artist: "Ed Sheeran", image: "/images/COVER3.jpeg", audio: "/audio/song3.mp3" },
-    { id: 4, title: "Someone Like You", artist: "Adele", image: "/images/COVER4.jpeg", audio: "/audio/song4.mp3" },
-    { id: 5, title: "Levitating", artist: "Dua Lipa", image: "/images/COVER3.jpeg", audio: "/audio/song5.mp3" },
-    { id: 6, title: "Circles", artist: "Post Malone", image: "/images/COVER2.jpeg", audio: "/audio/song6.mp3" },
-    { id: 7, title: "Stay", artist: "Rihanna", image: "/images/COVER1.jpeg", audio: "/audio/song7.mp3" },
-    { id: 8, title: "Perfect", artist: "Ed Sheeran", image: "/images/COVER4.jpeg", audio: "/audio/song2.mp3" },
-    { id: 9, title: "Dance Monkey", artist: "Tones and I", image: "/images/song5.jpeg", audio: "/audio/song1.mp3" },
+    const songs = [
+    { id: 1, title: "Lost Stars", artist: "Adam Levine", image: "images/song1.jpeg", audio: "audio/song2.mp3" },
+    { id: 2, title: "Blinding Lights", artist: "The Weeknd", image: "images/COVER2.jpeg", audio: "audio/song1.mp3" },
+    { id: 3, title: "Shape of You", artist: "Ed Sheeran", image: "images/COVER3.jpeg", audio: "audio/song3.mp3" },
+    { id: 4, title: "Someone Like You", artist: "Adele", image: "images/COVER4.jpeg", audio: "audio/song4.mp3" },
+    { id: 5, title: "Levitating", artist: "Dua Lipa", image: "images/COVER3.jpeg", audio: "audio/song5.mp3" },
+    { id: 6, title: "Circles", artist: "Post Malone", image: "images/COVER2.jpeg", audio: "audio/song6.mp3" },
+    { id: 7, title: "Stay", artist: "Rihanna", image: "images/COVER1.jpeg", audio: "audio/song7.mp3" },
+    { id: 8, title: "Perfect", artist: "Ed Sheeran", image: "images/COVER4.jpeg", audio: "audio/song2.mp3" },
+    { id: 9, title: "Dance Monkey", artist: "Tones and I", image: "images/song5.jpeg", audio: "audio/song1.mp3" },
   ];
 
    
