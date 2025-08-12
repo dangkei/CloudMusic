9965245799bc472e341fa02c2e82b2ff03b4e17e
dangkei <dangkei@163.com>
2025-08-12 11:18:21 +0800

    修改main index文件

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
+    <BrowserRouter basename="/CloudMusic"
       <App />
     </BrowserRouter>
   </React.StrictMode>
